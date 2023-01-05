00c19e584164ae05407b067206cc384148b4dc50
Jonathan Silva <jonathan.silva@smar.com.br>
2023-01-05 14:05:45 -0300

    chore: create dunder init file for main project

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+Entry point for Sentence Miner add-on from Anki.
+
+Performs any migration tasks and then loads the 'sentminer' package.
+"""
+
+from sys import stderr
+
+
+if __name__ == "__main__":
+    stderr.write(
+        "sentminer is a sentence miner add-on for Anki.\n"
+        "To learn more or download Anki, visit <https://apps.ankiweb.net>.\n"
+    )
+    exit(1)
